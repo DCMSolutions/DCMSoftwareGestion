81ce790c36f18c633b9574466e9b67214be5efbc
Petiso13 <ian13celestino@gmail.com>
2025-07-14 12:47:22 -0300

    Flujo funcional con el envío de mail después de primer y segundo uso de tokens

---
+++ src/app/api/webhooks/lockers/helpers.ts
@@ -0,0 +1,107 @@
+import { TokenRequestCreationBody, TokenRequestEditionBody } from "./types";
+import sendgrid from "@sendgrid/mail"
+import { Html, render } from "@react-email/components"
+import QRCode from "qrcode";
+import { env } from "~/env";
+
+const base_url = 'https://testing.server.dcm.com.ar/api/v2/token'
+
+export async function addTokenToServer(token: TokenRequestCreationBody, lockerSerial: string, bearerToken: string) {
+	const response = await fetch(`${base_url}/${lockerSerial}`,
+		{
+			method: 'POST',
+			headers: {
+				'Authorization': `Bearer ${bearerToken}`,
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(token)
+		});
+	return response
+}
+
+export async function editTokenToServer(token: TokenRequestEditionBody, lockerSerial: string, bearerToken: string) {
+	const response = await fetch(`${base_url}/${lockerSerial}`,
+		{
+			method: 'PUT',
+			headers: {
+				'Authorization': `Bearer ${bearerToken}`,
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(token)
+		});
+	return response
+}
+
+export async function sendAfterFirstUseEmail({ to, lockerAddress, checkoutTime, userToken }: { to: string, lockerAddress: string, checkoutTime: string, userToken: string }) {
+
+	// const qrCode = await QRCode.toDataURL(userToken);
+
+	sendgrid.setApiKey(env.SENDGRID_API_KEY);
+	const msg = {
+		to,
+		from: env.MAIL_SENDER,
+		subject: 'PICKPACK: Paquete listo para ser recogido',
+		html: `
+			<body>
+
+				<p>El paquete destinado a su locker reservado en ${lockerAddress} fue entregado. Le recordamos que el tiempo límite para pasarlo a buscar es ${checkoutTime}</p>
+
+				<p><strong>Su código de acceso (Token) para el locker que contiene su paquete es ${userToken}</strong></p>
+
+				<p>Atentamente,</p>
+				<p>el equipo de <strong>PickPack</strong></p>
+
+			</body>`
+		// attachments: [
+		// 	{
+		// 		filename: `${userToken}.png`,
+		// 		content: qrCode,
+		// 		type: 'image/png',
+		// 		disposition: 'attachment'
+		// 	}
+		// ],
+	};
+	sendgrid.send(msg)
+		.then(() => {
+			console.log("Email sent");
+		})
+		.catch((e: any) => {
+			console.log(e);
+		});
+}
+
+export async function sendGoodbyeEmail({ to }: { to: string }) {
+
+	// const qrCode = await QRCode.toDataURL(userToken);
+
+	sendgrid.setApiKey(env.SENDGRID_API_KEY);
+	const msg = {
+		to,
+		from: env.MAIL_SENDER,
+		subject: 'PICKPACK: paquete recogido',
+		html: `
+			<body>
+
+				<p>Gracias por confiar en <strong>PickPack</strong>, esperamos que nuestro servicio haya sido de su agrado.</p>
+
+				<p>Atentamente,</p>
+				<p>el equipo de <strong>PickPack</strong></p>
+
+			</body>`
+		// attachments: [
+		// 	{
+		// 		filename: `${userToken}.png`,
+		// 		content: qrCode,
+		// 		type: 'image/png',
+		// 		disposition: 'attachment'
+		// 	}
+		// ],
+	};
+	sendgrid.send(msg)
+		.then(() => {
+			console.log("Email sent");
+		})
+		.catch((e: any) => {
+			console.log(e);
+		});
+}
---
+++ src/app/api/webhooks/lockers/route.ts
@@ -0,0 +1,73 @@
+import { NextRequest, NextResponse } from "next/server";
+import { EVENTS, type TokenUseResponseData, type LockerWebhook, TokenRequestCreationBody, TokenRequestEditionBody } from "./types";
+import { db } from "~/server/db";
+import { Reserve } from "~/server/api/routers/reserves";
+import { reservas } from "~/server/db/schema";
+import { eq } from "drizzle-orm";
+import { addTokenToServer, editTokenToServer, sendAfterFirstUseEmail, sendGoodbyeEmail } from "./helpers";
+
+const bearer_token = 'xkg3tQKqSu1PvaldmTmKR'
+const locker_serial = 'TEST-IAN'
+
+export async function POST(request: NextRequest) {
+	const wh: LockerWebhook = await request.json()
+	console.log(wh)
+	if (wh.evento === EVENTS.TOKEN_USE_RESPONSE) {
+		const whData: TokenUseResponseData = JSON.parse((wh.data as unknown) as string)
+		if (whData.Respuesta === 'Aceptado') {
+			const lockerReservations: Reserve[] = await db.select().from(reservas).where(eq(reservas.NroSerie, wh.nroSerieLocker))
+			let reservation = lockerReservations.find(reservation => reservation.Token2 === parseInt(whData.Token))
+			if (reservation) {
+				await sendGoodbyeEmail({ to: reservation.client! })
+				return NextResponse.json({ status: 200 })
+			}
+			reservation = lockerReservations.find(reservation => reservation.Token1 === parseInt(whData.Token))
+			await db.update(reservas).set({ IdBox: whData.Box }).where(eq(reservas.identifier, reservation?.identifier!))
+			console.log(reservation)
+			if (reservation?.Token2) {
+				return NextResponse.json({ status: 200 })
+			}
+			const webhookEventTime = new Date(wh.fechaCreacion)
+			webhookEventTime.setMinutes(webhookEventTime.getMinutes() + 1 - (180))
+			let newLimit = webhookEventTime.toISOString().split('.')[0]
+			console.log(newLimit)
+			const tokenEditBody: TokenRequestEditionBody = {
+				token1: reservation?.Token1!.toString()!,
+				fechaFin: newLimit,
+				idBox: -1,
+			}
+			const editToken1Response = await editTokenToServer(tokenEditBody, locker_serial, bearer_token)
+			if (!editToken1Response.ok) {
+				console.log('No se pudo editar la fecha fin del token de primer uso sumándole 15 minutos')
+				const error = await editToken1Response.text()
+				console.log(error)
+			}
+			webhookEventTime.setMinutes(webhookEventTime.getMinutes() + 1)
+			newLimit = webhookEventTime.toISOString().split('.')[0]
+			const newToken: TokenRequestCreationBody = {
+				idSize: reservation?.IdSize!,
+				idBox: whData.Box!,
+				fechaInicio: newLimit,
+				fechaFin: reservation?.FechaFin!,
+				confirmado: true
+			}
+			console.log(newToken)
+			const token2Response = await addTokenToServer(newToken, locker_serial, bearer_token)
+			if (!token2Response.ok) {
+				const token2 = await token2Response.text()
+				console.log(token2)
+			} else {
+				const token2 = await token2Response.text()
+				console.log(token2)
+				await sendAfterFirstUseEmail({
+					to: reservation?.client!,
+					checkoutTime: reservation?.FechaFin!,
+					userToken: token2,
+					lockerAddress: reservation?.NroSerie!
+				})
+				await db.update(reservas).set({ Token2: parseInt(token2) }).where(eq(reservas.identifier, reservation?.identifier!))
+			}
+		}
+	}
+	return NextResponse.json({ status: 200 })
+}
